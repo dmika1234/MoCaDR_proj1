411e56ed6a1b25388f818d4d50053bd6af5025cd
Dominik <dmika1234@gmail.com>
2022-03-11 21:01:28 +0100

    Cleaning data

---
+++ manipulating_datasets.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import random as rng
+
+raw_df = pd.read_csv('Datasets/ratings_dirt.csv')
+df = raw_df.drop('timestamp', axis=1)
+df.to_csv('Datasets/ratings.csv')
+
+# Take some small subset of our data for testing purposes
+rand_ix = rng.sample(list(np.unique(df['userId'].values)), 10)
+sub_df = df.loc[df['userId'].isin(rand_ix)]
+sub_df.to_csv('Datasets/small_ratings.csv')
