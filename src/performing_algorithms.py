51e617278b26f52a55cef4d0b838cf795132aa0f
tamarafraczek <t.fraczek@g.loiv.torun.pl>
2022-03-20 14:48:35 +0100

    SVD opis Tamarka

--- src/performing_algorithms.py
+++ src/performing_algorithms.py
@@ -49,15 +49,54 @@ if len(movies_ids_miss2) != 0:
 else:
     test_array = test_wide
 
-
-
-
-
 #             Imputation
 
 # Imputing with 0
 train_df.fillna(0)
 train_array = np.array(train_df.fillna(0))
 
-results = perform_svd1(train_array, test_array, 2)
-print(results)
+results = perform_svd1(train_array, test_array, 10)
+# Imputing with global mean
+global_mean = train_df.mean().mean()
+train_array2 = np.array(train_df.fillna(global_mean))
+results2 = perform_svd1(train_array2, test_array, 10)
+
+
+# Imputing with column means
+# train_array3 = train_df.fillna(train_df.mean())
+# train_array3 = np.array(train_array3.T.fillna(train_array3.mean(axis=1)).T)
+# results3 = perform_svd1(train_array3, test_array, 10)
+
+# Imputing with row means
+train_array4 = np.array(train_df.T.fillna(train_df.mean(axis=1)).T)
+results4 = perform_svd1(train_array4, test_array, 10)
+
+# Imputing with combined row and column means
+col_means = np.matrix(train_df.mean().values)
+row_means = np.matrix(train_df.mean(axis=1).values).T
+#   Creating custom values to fill NaN in train_df
+fill_matrix = np.dot(row_means,  col_means / global_mean)
+fill_df = pd.DataFrame(fill_matrix)
+fill_df = fill_df.T.fillna(fill_df.mean(axis=1)).T
+fill_array = np.array(fill_df)
+na_indx = train_df.isna()
+train_array5 = np.array(train_df)
+train_array5[na_indx] = np.array(fill_array[na_indx]).reshape(-1)
+results5 = perform_svd1(train_array5, test_array, 20)
+
+# Imputing with weighted row and column means
+wages = np.array([0.6, 0.4])
+fill_matrix2 = np.log(np.dot(np.exp(wages[0] * row_means), np.exp(wages[1] * col_means)))
+fill_df2 = pd.DataFrame(fill_matrix2)
+fill_df2 = fill_df2.T.fillna(fill_df2.mean(axis=1)).T
+fill_array2 = np.array(fill_df2)
+train_array6 = np.array(train_df)
+train_array6[na_indx] = np.array(fill_array2[na_indx]).reshape(-1)
+results6 = perform_svd1(train_array6, test_array, 10)
+
+print('0: ', results)
+print('Global mean: ', results2)
+# print('Column means: ', results3)
+print('Row means: ', results4)
+print('Combined means: ', results5)
+print('Weighted means: ', results6)
